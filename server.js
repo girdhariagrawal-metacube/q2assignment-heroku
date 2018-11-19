6815cd0b08a415e52ddcc8c176d949c24a525277
Girdhari Agrawal <girdhari.agrawal@metacube.com>
2018-11-19 13:01:21 +0530

    Deployment issue

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 app.search(express.static(__dirname + '/dist'));
 app.listen(process.env.PORT || 8080);
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+
+console.log('Console Listening');
