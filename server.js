c8712a4c42174161d365ebb38bef97bca5aae09f
Girdhari Agrawal <girdhari.agrawal@metacube.com>
2018-11-19 13:11:18 +0530

    Deployment issue

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require('path');
 
 app.search(express.static(__dirname + '/dist/q2assignment-heroku'));
-app.listen(process.env.PORT || 8083);
+app.listen(process.env.PORT || 8080);
 
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname + '/dist/q2assignment-heroku/index.html'));
